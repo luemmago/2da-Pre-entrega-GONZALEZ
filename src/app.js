64e1667b69431ba552c289fd014d25c2a1e3cb7c
Emmanuel Gonzalez <L_EmmanuelGonzalez@outlook.com>
2023-06-08 23:10:52 -0300

    .

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
-import express  from 'express'
+import express from 'express'
+import 'dotenv/config.js'
+import router from './router/index.js'
+import error_handler from './middlewares/error.js'
+import not_found_handler from './middlewares/notfound.js'
 
 const server = express()
 
 //middlewares
-server.use('/public', express.static('public'))
+server.use('/public',express.static('public'))
 server.use(express.json())
-server.use(express.urlencoded({ extended:true}))
+server.use(express.urlencoded({extended:true}))
 
+//endpoints
+server.use('/api',router)
 server.use(error_handler)
 server.use(not_found_handler)
 
