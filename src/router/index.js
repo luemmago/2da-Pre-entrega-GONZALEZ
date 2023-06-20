b10b614de387463931c553375d6b0bcf0cce42cc
Emmanuel Gonzalez <L_EmmanuelGonzalez@outlook.com>
2023-06-20 18:21:38 -0300

    sess

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,15 @@ import { Router } from "express";
 import movies_router from "./movies.js";
 import pizzas_router from "./pizzas.js"
 import carts_router from "./carts.js"
+import cookies_router from "./cookies.js"
+import sessions_router from "./sessions.js"
 
 const router = Router()
 
 router.use('/movies',movies_router)
 router.use('/pizzas',pizzas_router)
 router.use('/carts',carts_router)
+router.use('/cookies',cookies_router)
+router.use('/sessions',sessions_router)
 
 export default router
