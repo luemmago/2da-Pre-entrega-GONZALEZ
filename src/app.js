5beb33b402f6ddf5691f9e9b1e3de57e03f186cd
Emmanuel Gonzalez <L_EmmanuelGonzalez@outlook.com>
2023-06-29 00:33:33 -0300

    pizza

--- src/app.js
+++ src/app.js
@@ -5,9 +5,13 @@ import morgan from 'morgan'
 const server = express()
 
 //middleware
+server.use('/', express.static('public'))
+server.use('/otra', express.static('otra'))
 server.use(express.json())
 server.use(express.urlencoded({ extended:true}))
 server.use(morgan('dev'))
+
+
 
 
 export default server
