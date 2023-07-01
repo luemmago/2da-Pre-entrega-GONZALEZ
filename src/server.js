7ab3c4b626334848cf5b59a5fb73cf046f2af05a
Emmanuel Gonzalez <L_EmmanuelGonzalez@outlook.com>
2023-07-01 18:51:42 -0300

    fix

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 import server from "./app.js"
-import { connect } from "mongoose"
+import { connect } from "mongoose";
 
 const port = process.env.PORT || 8080
 const ready = () => {
-    console.log('server ready on port ' + port)
+    console.log('server ready on port ' + port);
     connect(process.env.LINK_MONGO)
-        .then(() =>console.log('connected to database'))
-        .catch(() => console.log(err))
+        .then(() => console.log('connected to database'))
+        .catch(err => console.log(err))
 }
 
 server.listen(port, ready)
