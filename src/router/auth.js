9b0902316dacb27aeeee9ca28bbfc2243f93d0a5
Emmanuel Gonzalez <L_EmmanuelGonzalez@outlook.com>
2023-07-02 16:01:03 -0300

    .

--- src/router/auth.js
+++ src/router/auth.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
-import User from '../models/User'
-import validator from "../middlewares/validator";
-import pass_is_8 from "../middlewares/pass_is_8";
+import User from '../models/User.js'
+import validator_register from "../middlewares/validator.js";
+import validator_signin from "../middlewares/validator_Signin.js"
+import pass_is_8 from "../middlewares/pass_is_8.js";
 
 const auth_router = Router()
 
 //register routes
-auth_router.post('/register',validator,pass_is_8, async (req, res,next) => {
+auth_router.post('/register',
+    validator_register,
+    pass_is_8,
+    create_hash, 
+    async (req, res,next) => {
     try {
         await User.create(req.body)
         return res.status(201).json({
