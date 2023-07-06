192bbeb0806947c5a483e4ca2c5ac36a4a87710d
Emmanuel Gonzalez <L_EmmanuelGonzalez@outlook.com>
2023-07-05 23:34:36 -0300

    .

--- src/routes/api/auth.js
+++ src/routes/api/auth.js
@@ -6,68 +6,20 @@ import pass_is_8 from "../../middlewares/pass_is_8.js";
 import isValidPassword from "../../middlewares/isValidPassword.js";
 import passport from "passport";
 
-const auth_router = Router()
+const router = Router()
 
-//register routes
-auth_router.post('/register',
-    validator_register,
-    pass_is_8,
-    create_hash,
-    passport.authenticate(
-        'register',
-        { failureRedirect: '/api/auth/fail-register' }
-    ),
-    (req, res,) => res.status(201).json({
-        success: true,
-        message: 'user created!'
-    })
-
-
+router.get('/github', passport.authenticate('github', { scope:['user:email']}), (req,res)=>{})
+router.get(
+    'github/callback',
+    passport.authenticate('github', { failureRedirect:'/api/auth/fail-register-github'}),
+    (req,res)=> res.status(200).redirect('/)')
 )
-
-auth_router.get('/fail-register', (req, res) => res.status(400).json({
+router.get('fail-register-github', (req, res)=> res.status(403).json({
     success: false,
-    message: 'error auth'
+    message: 'bad auth'
 }))
 
-auth_router.post('/signin',
-    validator_signin,
-    pass_is_8,
-    passport.authenticate('signin', { failureRedirect: '/api/auth/fail-signin' }),
-    isValidPassword,
-    (req, res, next) => {
-        try {
-            const { email } = req.body
-            req.session.email = email
-            req.session.role = req.user.role
-            return res.status(200).json({
-                success: true,
-                message: 'user signed in!'
-            })
+export default router
 
 
-        } catch (error) {
-            next(error)
-        }
-    }
-)
 
-auth_router.get('/fail-signin', (req, res) => res.status(400).json({
-    success: false,
-    message: 'error auth'
-}))
-
-//sinout
-auth_router.post('signout', (req, res, next) => {
-    try {
-        req.session.destroy()
-        return res.status(200).json({
-            success: true,
-            message: 'User signed out'
-        })
-    } catch (error) {
-        next(error)
-    }
-})
-
-export default auth_router
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,14 @@
 import { Router } from "express"
+import products_router from "./api/products"
 import api_router from './api/index.js'
+import carts_router from './api/carts'
+import auth_router from "./api/auth"
 
 const index_router = Router()
 
+router.use('/products', products_router)
+router.use('/carts', carts_router)
+router.use('/auth', auth_router)
 index_router.use('/api',api_router) //enrutador de rutas que respondan con json (datos)
 
 export default index_router
