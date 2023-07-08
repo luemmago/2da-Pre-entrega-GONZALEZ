520f27ad815a3ee9f37a6a6e9a217934c6a884a9
Emmanuel Gonzalez <L_EmmanuelGonzalez@outlook.com>
2023-07-08 01:45:05 -0300

    .

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,8 @@
-import { dirname } from 'path'
+import {dirname} from 'path'
 import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
-//console.log(__dirname)
+console.log(__dirname)
 
-export { __filename, __dirname }
-//__dirname es la ruta principal "base"
-//hasta que hosteemos nuestro servidor la semana que viene
-//__dirname es localhost8080
+export {__filename, __dirname}
