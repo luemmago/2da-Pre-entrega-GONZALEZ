6b87eafe97d92c0614653780ca1f55c504072795
Emmanuel Gonzalez <L_EmmanuelGonzalez@outlook.com>
2023-07-09 16:31:26 -0300

    .

--- src/middlewares/generateToken.js
+++ src/middlewares/generateToken.js
@@ -3,8 +3,8 @@ import jwt from 'jsonwebtoken'
 export default (req,res,next) => {
     req.token = jwt.sign(
         { email:req.body.email },
-        process.env.SECRET,
-        { expiresIn:60*60*24 }
+        process.env.SECRET_JWT,
+        { expiresIn:60*60*24*7 }
     )
     return next()
 }
